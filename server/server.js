2faf04ee5c0a439c4836a9dff1e34d522fd078ab
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-11-16 19:58:33 +0530

    database connected

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const app=require('./app')
 const dotenv=require("dotenv")
+const connectToDB=require("./config/database")
 
 dotenv.config({path:"server/config/config.env"})
+
+connectToDB()
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is working on port http://localhost:${process.env.PORT}`)
