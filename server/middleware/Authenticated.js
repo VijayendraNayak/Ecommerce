1163f9f68269b4b16f211d993966874faf21ee29
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-11-23 21:00:14 +0530

    isAuth and authrole

--- server/middleware/Authenticated.js
+++ server/middleware/Authenticated.js
@@ -1,4 +1,4 @@
-const  jwt  = require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 const User = require("../models/usermodel")
 const { errorHandler } = require("../utils/errorHandler")
 const { asyncErrHandler } = require("./asyncerrorHandler")
@@ -7,6 +7,15 @@ exports.isAuthenticated = asyncErrHandler(async (req, res, next) => {
     const { access_token } = req.cookies
     if (!access_token) { return next(errorHandler(400, "Login to view this page")) }
     const isVerified = jwt.verify(access_token, process.env.JWT_SECRET)
-    req.user=await User.findById(isVerified.id)
+    req.user = await User.findById(isVerified.id)
     next()
 })
+
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(errorHandler(403, "You should be an admin to use this resource"))
+        }
+        next()
+    }
+}
--- server/routes/productrouter.js
+++ server/routes/productrouter.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { getallproducts, createProduct, updateProduct, deleteProduct,getProduct ,getSearch} = require("../controllers/productcontroller");
-const { isAuthenticated } = require("../middleware/Authenticated");
+const { isAuthenticated, authorizeRoles } = require("../middleware/Authenticated");
 const router = express.Router();
 
-router.get("/getallproducts",isAuthenticated,getallproducts)
-router.post("/create", createProduct)
-router.put("/update/:id", updateProduct)
-router.delete("/delete/:id", deleteProduct)
+router.get("/getallproducts",getallproducts)
+router.post("/create",isAuthenticated,authorizeRoles("admin"), createProduct)
+router.put("/update/:id",isAuthenticated,authorizeRoles("admin"), updateProduct)
+router.delete("/delete/:id",isAuthenticated,authorizeRoles("admin"), deleteProduct)
 router.get("/getproduct/:id",getProduct)
 router.get("/get",getSearch)
 
