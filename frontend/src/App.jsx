e979fb785695ae913e9fc95911bbea3fba7eb9ea
Vijayendra Nayak <91378323+VijayendraNayak@users.noreply.github.com>
2023-11-29 15:28:25 +0530

    frontend pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
-const App = () => {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+export default function App() {
   return (
-    <div className='text-red-500'>
-      hello
-    </div>
-  )
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sign-in" element={<Login/>}/>
+        <Route path="/sign-up" element={<Register/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
